3bdc6d7bbf2da8ceb5fb23dd73edcf68e1704ea5
Seifallah Azzabi <seifallah.azzabi@gmail.com>
2018-06-05 10:25:56 +0200

    Initial Commit

---
+++ cache-acl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
+
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
---
+++ install-dotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ~/.bashrc ~/.bashrc_back
+cp .bashrc ~/.bashrc
+cp .bashrc_coloration.sh ~/.bashrc_coloration.sh
+cp .gitconfig ~/.gitconfig
